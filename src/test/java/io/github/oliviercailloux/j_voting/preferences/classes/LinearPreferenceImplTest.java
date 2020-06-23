cfa4266c3d208f9dbb916b821702e9f160576173
Julienchilhagopian <julienchilhagopian@gmail.com>
2020-06-23 22:42:48 +0200

    FORMAT

--- src/test/java/io/github/oliviercailloux/j_voting/preferences/classes/LinearPreferenceImplTest.java
+++ src/test/java/io/github/oliviercailloux/j_voting/preferences/classes/LinearPreferenceImplTest.java
@@ -16,51 +16,45 @@ import io.github.oliviercailloux.j_voting.preferences.interfaces.LinearPreferenc
 
 class LinearPreferenceImplTest {
 
-    private static Voter v1 = Voter.createVoter(1);
+	private static Voter v1 = Voter.createVoter(1);
 
-    private static LinearPreference getListedAlternatives()
-                    throws EmptySetException, DuplicateValueException {
-        return LinearPreferenceImpl.asLinearPreference(Voter.createVoter(3),
-                        ImmutableList.of(a1, a3, a2));
-    }
+	private static LinearPreference getListedAlternatives() throws EmptySetException, DuplicateValueException {
+		return LinearPreferenceImpl.asLinearPreference(Voter.createVoter(3), ImmutableList.of(a1, a3, a2));
+	}
 
-    @Test
-    void testlistToLinearPreference()
-                    throws EmptySetException, DuplicateValueException {
-        LinearPreference toTest = getListedAlternatives();
-        assertEquals(ImmutableList.of(a1, a3, a2), toTest.asList());
-    }
+	@Test
+	void testlistToLinearPreference() throws EmptySetException, DuplicateValueException {
+		LinearPreference toTest = getListedAlternatives();
+		assertEquals(ImmutableList.of(a1, a3, a2), toTest.asList());
+	}
 
-    @Test
-    void testAsList() throws EmptySetException, DuplicateValueException {
-        LinearPreference toTest = getListedAlternatives();
-        assertEquals(ImmutableList.of(a1, a3, a2), toTest.asList());
-    }
-    
-    @Test
-    void equalsTest() throws Exception {
-    	LinearPreference toTest = getListedAlternatives();
-    	LinearPreference toTest2 = LinearPreferenceImpl.asLinearPreference(Voter.createVoter(3), ImmutableList.of(a1, a2, a3));
-    	LinearPreference toTest3 = LinearPreferenceImpl.asLinearPreference(Voter.createVoter(1), ImmutableList.of(a1, a3, a2));
-    	LinearPreference toTest4 = LinearPreferenceImpl.asLinearPreference(Voter.createVoter(3), ImmutableList.of(a1, a3, a2));
-        assertNotEquals(toTest, toTest2);
-        assertNotEquals(toTest, toTest3);
-        assertEquals(toTest, toTest4);
-    }
+	@Test
+	void testAsList() throws EmptySetException, DuplicateValueException {
+		LinearPreference toTest = getListedAlternatives();
+		assertEquals(ImmutableList.of(a1, a3, a2), toTest.asList());
+	}
 
-    @Test
-    void throwsTest() {
-        assertThrows(Exception.class, () -> LinearPreferenceImpl
-                        .asLinearPreference(null, ImmutableList.of(null)));
-        assertThrows(Exception.class, () -> LinearPreferenceImpl
-                        .asLinearPreference(v1, ImmutableList.of(null)));
-        assertThrows(Exception.class, () -> LinearPreferenceImpl
-                        .asLinearPreference(v1, null));
-        assertThrows(Exception.class,
-                        () -> LinearPreferenceImpl.asLinearPreference(
-                                        Voter.createVoter(1),
-                                        ImmutableList.of(null)));
-        assertThrows(Exception.class, () -> LinearPreferenceImpl
-                        .asLinearPreference(v1, null));
-    }
+	@Test
+	void equalsTest() throws Exception {
+		LinearPreference toTest = getListedAlternatives();
+		LinearPreference toTest2 = LinearPreferenceImpl.asLinearPreference(Voter.createVoter(3),
+				ImmutableList.of(a1, a2, a3));
+		LinearPreference toTest3 = LinearPreferenceImpl.asLinearPreference(Voter.createVoter(1),
+				ImmutableList.of(a1, a3, a2));
+		LinearPreference toTest4 = LinearPreferenceImpl.asLinearPreference(Voter.createVoter(3),
+				ImmutableList.of(a1, a3, a2));
+		assertNotEquals(toTest, toTest2);
+		assertNotEquals(toTest, toTest3);
+		assertEquals(toTest, toTest4);
+	}
+
+	@Test
+	void throwsTest() {
+		assertThrows(Exception.class, () -> LinearPreferenceImpl.asLinearPreference(null, ImmutableList.of(null)));
+		assertThrows(Exception.class, () -> LinearPreferenceImpl.asLinearPreference(v1, ImmutableList.of(null)));
+		assertThrows(Exception.class, () -> LinearPreferenceImpl.asLinearPreference(v1, null));
+		assertThrows(Exception.class,
+				() -> LinearPreferenceImpl.asLinearPreference(Voter.createVoter(1), ImmutableList.of(null)));
+		assertThrows(Exception.class, () -> LinearPreferenceImpl.asLinearPreference(v1, null));
+	}
 }
